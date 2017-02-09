825d60a713e618281514bb176d89eefe39e74d32
Michael COULLERET <michael@coulleret.pro>
2017-02-09 23:07:44 +0100

    Fix/insight (#7)
    * [Fix] insight

---
+++ Resources/Manager/SupervisorManager.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the OsLabSupervisorBundle package.
+ *
+ * (c) OsLab <https://github.com/OsLab>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OsLab\SupervisorBundle\DependencyInjection;
+
+class SupervisorManager
+{
+}
